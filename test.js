360686a5f967d1ad3521982eb6fb11d541b304c2
Anleb <an.wu@baoz.cn>
2017-05-27 08:45:14 +0800

    1. format code: Delimiter change `Tab`

--- test.js
+++ test.js
@@ -6,285 +6,285 @@ var coroutine = require("coroutine");
 var Pool = require(".");
 
 describe("pool", () => {
-    it("run", () => {
-        var p = Pool(() => {
-            return 10;
-        });
+	it("run", () => {
+		var p = Pool(() => {
+			return 10;
+		});
 
-        assert.equal(p((v) => {
-            return v + 1;
-        }), 11);
-    });
+		assert.equal(p((v) => {
+			return v + 1;
+		}), 11);
+	});
 
-    it("pool", () => {
-        var n = 0;
+	it("pool", () => {
+		var n = 0;
 
-        var p = Pool(() => {
-            n++;
-            return n;
-        });
+		var p = Pool(() => {
+			n++;
+			return n;
+		});
 
-        assert.equal(p((v) => {
-            return v + 1;
-        }), 2);
-    });
+		assert.equal(p((v) => {
+			return v + 1;
+		}), 2);
+	});
 
-    it("maxsize", () => {
-        var n = 0;
-        var m = 0;
+	it("maxsize", () => {
+		var n = 0;
+		var m = 0;
 
-        var p = Pool(() => {
-            // n++;
-            return n;
-        }, 10);
+		var p = Pool(() => {
+			// n++;
+			return n;
+		}, 10);
 
-        coroutine.parallel(() => {
-            p((c) => {
-                n++;
-                if (n > m)
-                    m = n;
-                coroutine.sleep(50);
-                n--;
-            });
-        }, 20);
+		coroutine.parallel(() => {
+			p((c) => {
+				n++;
+				if (n > m)
+					m = n;
+				coroutine.sleep(50);
+				n--;
+			});
+		}, 20);
 
-        assert.equal(m, 10);
-    });
+		assert.equal(m, 10);
+	});
 
-    it("name", () => {
-        var n = 0;
+	it("name", () => {
+		var n = 0;
 
-        var p = Pool((name) => {
-            n++;
-            return name;
-        });
+		var p = Pool((name) => {
+			n++;
+			return name;
+		});
 
-        assert.equal(n, 0);
+		assert.equal(n, 0);
 
-        assert.equal(p('a', (v) => {
-            return v;
-        }), 'a');
+		assert.equal(p('a', (v) => {
+			return v;
+		}), 'a');
 
-        assert.equal(n, 1);
+		assert.equal(n, 1);
 
-        assert.equal(p('b', (v) => {
-            return v;
-        }), 'b');
+		assert.equal(p('b', (v) => {
+			return v;
+		}), 'b');
 
-        assert.equal(n, 2);
+		assert.equal(n, 2);
 
-        assert.equal(p('a', (v) => {
-            return v;
-        }), 'a');
+		assert.equal(p('a', (v) => {
+			return v;
+		}), 'a');
 
-        assert.equal(n, 2);
-    });
+		assert.equal(n, 2);
+	});
 
-    it("throw", () => {
-        var n = 0;
+	it("throw", () => {
+		var n = 0;
 
-        var p = Pool(() => {
-            n++;
-            return n;
-        });
+		var p = Pool(() => {
+			n++;
+			return n;
+		});
 
-        assert.equal(p((v) => {
-            return v + 1;
-        }), 2);
+		assert.equal(p((v) => {
+			return v + 1;
+		}), 2);
 
-        assert.throws(() => {
-            p((v) => {
-                throw "error";
-            });
-        });
+		assert.throws(() => {
+			p((v) => {
+				throw "error";
+			});
+		});
 
-        assert.equal(p((v) => {
-            return v + 1;
-        }), 3);
-    });
+		assert.equal(p((v) => {
+			return v + 1;
+		}), 3);
+	});
 
-    it("async close when throw", () => {
-        var called = false;
+	it("async close when throw", () => {
+		var called = false;
 
-        var p = Pool({
-            create: () => {
-                return 100;
-            },
-            destroy: (o) => {
-                called = true;
-            }
-        });
+		var p = Pool({
+			create: () => {
+				return 100;
+			},
+			destroy: (o) => {
+				called = true;
+			}
+		});
 
-        assert.throws(() => {
-            p((v) => {
-                throw "error";
-            });
-        });
+		assert.throws(() => {
+			p((v) => {
+				throw "error";
+			});
+		});
 
-        assert.isFalse(called);
-        coroutine.sleep(10);
-        assert.isTrue(called);
-    });
+		assert.isFalse(called);
+		coroutine.sleep(10);
+		assert.isTrue(called);
+	});
 
-    it("default close function", () => {
-        var called = false;
+	it("default close function", () => {
+		var called = false;
 
-        var p = Pool({
-            create: () => {
-                return {
-                    close: () => {
-                        called = true;
-                    }
-                };
-            }
-        });
+		var p = Pool({
+			create: () => {
+				return {
+					close: () => {
+						called = true;
+					}
+				};
+			}
+		});
 
-        assert.throws(() => {
-            p((v) => {
-                throw "error";
-            });
-        });
+		assert.throws(() => {
+			p((v) => {
+				throw "error";
+			});
+		});
 
-        assert.isFalse(called);
-        coroutine.sleep(10);
-        assert.isTrue(called);
-    });
+		assert.isFalse(called);
+		coroutine.sleep(10);
+		assert.isTrue(called);
+	});
 
-    it("default destroy function", () => {
-        var called = false;
+	it("default destroy function", () => {
+		var called = false;
 
-        var p = Pool({
-            create: () => {
-                return {
-                    destroy: () => {
-                        called = true;
-                    }
-                };
-            }
-        });
+		var p = Pool({
+			create: () => {
+				return {
+					destroy: () => {
+						called = true;
+					}
+				};
+			}
+		});
 
-        assert.throws(() => {
-            p((v) => {
-                throw "error";
-            });
-        });
+		assert.throws(() => {
+			p((v) => {
+				throw "error";
+			});
+		});
 
-        assert.isFalse(called);
-        coroutine.sleep(10);
-        assert.isTrue(called);
-    });
+		assert.isFalse(called);
+		coroutine.sleep(10);
+		assert.isTrue(called);
+	});
 
-    it("default dispose function", () => {
-        var called = false;
+	it("default dispose function", () => {
+		var called = false;
 
-        var p = Pool({
-            create: () => {
-                return {
-                    dispose: () => {
-                        called = true;
-                    }
-                };
-            }
-        });
+		var p = Pool({
+			create: () => {
+				return {
+					dispose: () => {
+						called = true;
+					}
+				};
+			}
+		});
 
-        assert.throws(() => {
-            p((v) => {
-                throw "error";
-            });
-        });
+		assert.throws(() => {
+			p((v) => {
+				throw "error";
+			});
+		});
 
-        assert.isFalse(called);
-        coroutine.sleep(10);
-        assert.isTrue(called);
-    });
+		assert.isFalse(called);
+		coroutine.sleep(10);
+		assert.isTrue(called);
+	});
 
-    it("clean timeout", () => {
-        var called = false;
+	it("clean timeout", () => {
+		var called = false;
 
-        var p = Pool({
-            create: () => {
-                return 100;
-            },
-            destroy: (o) => {
-                called = true;
-            },
-            timeout: 10
-        });
+		var p = Pool({
+			create: () => {
+				return 100;
+			},
+			destroy: (o) => {
+				called = true;
+			},
+			timeout: 10
+		});
 
-        p((v) => {});
+		p((v) => {});
 
-        assert.isFalse(called);
-        assert.equal(p.connections(), 1);
-        coroutine.sleep(100);
-        assert.isTrue(called);
-        assert.equal(p.connections(), 0);
-    });
+		assert.isFalse(called);
+		assert.equal(p.connections(), 1);
+		coroutine.sleep(100);
+		assert.isTrue(called);
+		assert.equal(p.connections(), 0);
+	});
 
-    it("retry", () => {
-        var n = 0;
+	it("retry", () => {
+		var n = 0;
 
-        var p = Pool(() => {
-            n++;
-            throw "open error";
-        });
+		var p = Pool(() => {
+			n++;
+			throw "open error";
+		});
 
-        assert.throws(() => {
-            p((v) => {});
-        });
+		assert.throws(() => {
+			p((v) => {});
+		});
 
-        assert.equal(n, 1);
+		assert.equal(n, 1);
 
-        var n1 = 0;
-        var p1 = Pool({
-            create: () => {
-                n1++;
-                throw "open error";
-            },
-            retry: 10
-        });
+		var n1 = 0;
+		var p1 = Pool({
+			create: () => {
+				n1++;
+				throw "open error";
+			},
+			retry: 10
+		});
 
-        assert.throws(() => {
-            p1((v) => {});
-        });
+		assert.throws(() => {
+			p1((v) => {});
+		});
 
-        assert.equal(n1, 10);
+		assert.equal(n1, 10);
 
-        var n2 = 0;
-        var p2 = Pool({
-            create: () => {
-                n2++;
-                if (n2 == 3)
-                    return;
-                throw "open error";
-            },
-            retry: 10
-        });
+		var n2 = 0;
+		var p2 = Pool({
+			create: () => {
+				n2++;
+				if (n2 == 3)
+					return;
+				throw "open error";
+			},
+			retry: 10
+		});
 
-        p2((v) => {});
+		p2((v) => {});
 
-        assert.equal(n2, 3);
-    });
+		assert.equal(n2, 3);
+	});
 
-    it("maxsize and create",function(){
-        var createCount = 0,
-            maxsize = 2;
+	it("maxsize and create", function() {
+		var createCount = 0,
+			maxsize = 2;
 
-        var p = Pool({
-            create: () => {
-                createCount++;
-                return {};
-            },
-            maxsize: maxsize
-        });
+		var p = Pool({
+			create: () => {
+				createCount++;
+				return {};
+			},
+			maxsize: maxsize
+		});
 
-        coroutine.parallel(["a","b","c","d"], function(n){
-            p((n) => {
-                coroutine.sleep(50);
-            })
-        });
+		coroutine.parallel(["a", "b", "c", "d"], function(n) {
+			p((n) => {
+				coroutine.sleep(50);
+			})
+		});
 
-        assert.equal(createCount, maxsize);
-    });
+		assert.equal(createCount, maxsize);
+	});
 });
 
 process.exit(test.run(console.DEBUG));
